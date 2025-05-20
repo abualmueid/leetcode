372311ae215f3e807520b20f7fd63c8d41b1c12c
Abu Al Mueid <61595430+abualmueid@users.noreply.github.com>
2025-05-20 21:06:18 +0600

    Added file

---
+++ blind-75/Blind75/TwoSum/Program.cs
@@ -0,0 +1,8 @@
+﻿using TwoSum;
+
+int[] nums = [2, 7, 11, 15];
+int target = 9;
+
+var solution = new Solution();
+int[] result = solution.TwoSum(nums, target);
+Console.WriteLine("[" + string.Join(",", result) + "]");
---
+++ blind-75/Blind75/TwoSum/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int index1 = 0, index2 = 0;
+            for (int i = 0; i < nums.Length - 1; i++) 
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        //return new int[] { i, j };
+                        index1 = i;
+                        index2 = j;
+                        break;
+                    }
+                }
+            }
+
+            return new int[] { index1, index2 };
+
+            //throw new ArgumentException("No two sum solution found.");
+        }
+    }
+}
