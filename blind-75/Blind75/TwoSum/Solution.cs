30c69af8f46ccf50619df649f45eb0ee35611c14
Abu Al Mueid <61595430+abualmueid@users.noreply.github.com>
2025-05-25 00:15:07 +0600

    Add two more solutions

--- blind-75/Blind75/TwoSum/Solution.cs
+++ blind-75/Blind75/TwoSum/Solution.cs
@@ -8,6 +8,9 @@ namespace TwoSum
 {
     public class Solution
     {
+        #region Solution 1
+        /*
+        // Time Complexity O(n2)
         public int[] TwoSum(int[] nums, int target)
         {
             int index1 = 0, index2 = 0;
@@ -29,5 +32,70 @@ namespace TwoSum
 
             //throw new ArgumentException("No two sum solution found.");
         }
+        */
+        #endregion
+
+        #region Solution 2
+        /*
+
+        // Time Complexity O(n), Space Complexity O(n)
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var map = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                map[nums[i]] = i;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+
+                if (map[complement] != i)
+                {
+                    if (map.ContainsKey(complement))
+                    {
+                        return new int[] { i, map[complement] };
+                    }
+                }
+            }
+
+            return new int[] { };
+        }
+        */
+
+
+
+        #endregion
+
+        #region Solution 3
+
+        // Time Complexity O(n), Space Complexity O(n)
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var map = new Dictionary<int, int>();
+            // for (int i = 0; i < nums.Length; i++)
+            // {
+            //     map[nums[i]] = i;
+            // }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                // map[nums[i]] = i;
+                int complement = target - nums[i];
+
+                if (map.ContainsKey(complement))
+                {
+                    return new int[] { i, map[complement] };
+                }
+                map[nums[i]] = i;
+            }
+
+            return new int[] { };
+        }
+        #endregion
     }
+
 }
